b73e469499f629252cad5dd77de2d40d1b6279f1
Emanuel Oliveira <emanuelgama219@gmail.com>
2025-12-04 16:46:32 -0400

    loginCliente

--- src/moduleRegisterClient.c
+++ src/moduleRegisterClient.c
@@ -1,28 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "moduleRegisterClient.h"
+//#include "moduleRegisterClient.h"
 
+
+typedef struct{
+  int id;
+  long pos;
+}Index;
+
+typedef struct{
+  char nome[20],email[25],senha[9],regiao;
+  int id;
+}Dados;
+
+void cadastroCliente(char *nome,char *email,char *senha,char regiao);
+void loginCliente(char *email,char *senha);
+int idCliente();
+void perfilCliente();
 static Dados userLog;
 
-void cadastroCliente(char *nome,char *email,char *senha,char regiao,int idade){
-  FILE *file = fopen("cliente.txt","a");
+
+void cadastroCliente(char *nome,char *email,char *senha,char regiao){
+  //gera um novo id
   int novoId = idCliente();
+  
+  //abre o arquivo cliente
+  FILE *file = fopen("cliente.txt","ab+");
+    
   if(file == NULL){
     printf("Erro ao abrir arquivo para escrita!");
      return;
   }
-  
+  //pega a ultima posiçao no arquivo
+  fseek(file,0,SEEK_END);
+  long posicao_registro = ftell(file);
+  //preenche a struct
   Dados data;
   strcpy(data.nome ,nome);
   strcpy(data.email,email);
   strcpy(data.senha ,senha);
   data.regiao = regiao;
-  data.idade = idade;
   data.id = novoId;
 
+  //escreve no arquivo e fecha
   fwrite(&data,sizeof(Dados),1,file);
   fclose(file);
+
+  FILE *fileIndex = fopen("clienteIndex.txt","ab");
+  if(fileIndex == NULL){
+    printf("Erro ao abrir o arquivo de indice para escrita");
+    return;
+  }
+  
+  Index idx;
+  idx.id = novoId;
+  idx.pos = posicao_registro;
+
+  fwrite(&idx,sizeof(Index),1,fileIndex);
+  fclose(fileIndex);
+
 }
 
 // gerador de id
@@ -40,7 +77,7 @@ int idCliente(){
 
   int proximo_id = ultimo_id + 1;
 
-  file = fopen("idCliente.txt","r");
+  file = fopen("idCliente.txt","w");
 
   if(file == NULL){
     printf("Nao foi possivel abrir o arquivo");
@@ -53,10 +90,11 @@ int idCliente(){
 }
 
 // 
-int loginCliente(char *email,char *senha){
-  FILE *file = fopen("cliente.txt","r");
+void loginCliente(char *email,char *senha){
+  FILE *file = fopen("cliente.txt","rb");
   if(file == NULL){
     printf("Erro ao abrir arquivo para leitura!");
+    return;
   }
   
   Dados data;
@@ -71,25 +109,56 @@ int loginCliente(char *email,char *senha){
       printf("Usuario encontrado, segue para pagina do cliente!");
       userLog = data;
       fclose(file);
-      return 1;
+      return;
+      
     }
   }  
     printf("Usuario nao encontrado ou nao cadastrado!");
     fclose(file); 
-    return 0;
+    
 }
 
 void perfilCliente(){
   printf("\n|----Dados do Perfil----|\n");
-  printf("|  Nome: %s\n  ",userLog.name);
-  printf("|  Email: %s\n  ",userLog.email);
-  printf("|  Idade: %d\n  ",userLog.idade);
-  printf("|  Regiao: %c\n  ",userLog.regiao);
-  printf("|  Senha: %s\n  ",userLog.regiao);
+  printf("|  Id: %d\n",userLog.id);
+  printf("|  Nome: %s\n",userLog.nome);
+  printf("|  Email: %s\n",userLog.email);
+  printf("|  Regiao: %c\n",userLog.regiao);
+  printf("|  Senha: %s\n",userLog.senha);
   printf("|-----------------------|\n");
 }
-
+/*
 void atualizarDados(){
 
-}
 
+}*/
+
+int main(){
+  char nome[] = "Emaniel ";
+  char email[] = "emanie@";
+  char regiao = 'r';
+  char senha[] = "1234";
+  char nome1[] = "jsoa ";
+  char email1[] = "jsiaj2";
+  char regiao1 = 's';
+  char senha1[] = "1234";
+  char nome2[] = "ganis ";
+  char email2[] = "ganis21";
+  char regiao2 = 'a';
+  char senha2[] = "1234";
+
+  printf("comecou");
+  cadastroCliente(nome,email,senha,regiao);
+  cadastroCliente(nome1,email1,senha1,regiao1);
+  cadastroCliente(nome2,email2,senha2,regiao2);
+  loginCliente(email,senha);
+  perfilCliente();
+  
+  loginCliente(email1,senha1);
+  perfilCliente();
+  
+  loginCliente(email2,senha2);
+  perfilCliente();
+
+  return 0;
+}
